9050319ba92a84afea0c3832f9028dd3e2f34dcb
KokulManivannan <kokulmanivannan@gmail.com>
2023-03-16 12:04:38 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://Mkokul:mgkp1234@cluster0.tt9iz6y.mongodb.net/?retryWrites=true&w=majority', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
